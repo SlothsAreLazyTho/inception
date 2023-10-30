cf36b0df942c8e4cdbd642666f286c449b7ed26a
SlothsAreLazyTho <chinobijman@gmail.com>
2023-10-30 15:18:17 +0100

    Lmfao

--- wordpress/tools/configure.sh
+++ wordpress/tools/configure.sh
@@ -1,3 +1,5 @@
+env | grep "DB"
+
 wp --allow-root core download --path=/var/www/html
 wp --allow-root config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --dbcharset='utf-8' --dbcollate="utf8_general_ci"
 wp --allow-root core install --url=localhost:8080 --title=Inception --admin_user=cbijman --admin_email=cbijman@student.codam.nl --admin_password=cbijman
