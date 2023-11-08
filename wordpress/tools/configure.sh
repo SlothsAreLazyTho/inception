76743e3f52dd08933ace2a2547ae064e2f14bf32
Chino <40154737+SlothsAreLazyTho@users.noreply.github.com>
2023-11-08 14:02:49 +0100

    More credentials oehh

--- wordpress/tools/configure.sh
+++ wordpress/tools/configure.sh
@@ -7,7 +7,7 @@ env | grep "DB"
 if [ ! -f "/var/www/html/wp-config.php"]; then
 	wp --allow-root core download --path=/var/www/html
 	wp --allow-root config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --dbcharset='utf-8' --dbcollate="utf8_general_ci"
-	wp --allow-root core install --url=localhost:8080 --title=Inception --admin_user=cbijman --admin_email=cbijman@student.codam.nl --admin_password=cbijman
+	wp --allow-root core install --url=$WP_DOMAIN:8080 --title=$WP_TITLE --admin_user=$WP_USER --admin_email=$WP_EMAIL --admin_password=$WP_PASS
 else
 	echo "Wordpress is installed!"
 end
