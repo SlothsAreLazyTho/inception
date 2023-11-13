ea39261bef14a075215162316be4266151faf5fe
Chino <40154737+SlothsAreLazyTho@users.noreply.github.com>
2023-11-13 16:16:50 +0100

    Inception done lol

--- mariadb/tools/configure.sh
+++ mariadb/tools/configure.sh
@@ -1,11 +1,23 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         ::::::::             #
+#    configure.sh                                       :+:    :+:             #
+#                                                      +:+                     #
+#    By: cbijman <cbijman@student.codam.nl>           +#+                      #
+#                                                    +#+                       #
+#    Created: 2023/11/13 16:12:30 by cbijman       #+#    #+#                  #
+#    Updated: 2023/11/13 16:12:31 by cbijman       ########   odam.nl          #
+#                                                                              #
+# **************************************************************************** #
+
 #!/bin/sh
 
 SQL_FILE_NAME="setup.sql"
 
 if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
     mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+    
     service mariadb start
-
     echo "CREATE USER \"$DB_USER\"@'%' IDENTIFIED BY \"$DB_PASS\";" >> $SQL_FILE_NAME
     echo "CREATE DATABASE $DB_NAME;" >> $SQL_FILE_NAME
     echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO \"$DB_USER\"@'%' WITH GRANT OPTION;" >> $SQL_FILE_NAME
@@ -13,9 +25,9 @@ if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 
     mysql < setup.sql
     echo "Successfully added $DB_USER to the database"
-    service mariadb stop
 
     # Cleaning inception
+    service mariadb stop
     rm -rf $SQL_FILE_NAME;
 else
     echo "MariaDB was already installed"
--- wordpress/tools/configure.sh
+++ wordpress/tools/configure.sh
@@ -1,10 +1,21 @@
-env | grep "DB"
+# **************************************************************************** #
+#                                                                              #
+#                                                         ::::::::             #
+#    configure.sh                                       :+:    :+:             #
+#                                                      +:+                     #
+#    By: cbijman <cbijman@student.codam.nl>           +#+                      #
+#                                                    +#+                       #
+#    Created: 2023/11/13 16:11:50 by cbijman       #+#    #+#                  #
+#    Updated: 2023/11/13 16:11:51 by cbijman       ########   odam.nl          #
+#                                                                              #
+# **************************************************************************** #
 
 # Wait till the database runs.
 
-
 # Attempt to install wordpress!
 if [ ! -f "/var/www/html/wp-config.php" ]; then
+	echo "Installing wordpress from the $(whoami) user!"
+
 	wp --allow-root core download --path=/var/www/html
 	wp --allow-root config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --dbcharset='utf8'
 	wp --allow-root core install --url=$WP_DOMAIN:8080 --title=$WP_TITLE --admin_user=$WP_USER --admin_email=$WP_EMAIL --admin_password=$WP_PASS
