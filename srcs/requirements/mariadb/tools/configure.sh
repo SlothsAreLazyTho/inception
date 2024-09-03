1896fd7e6e90b112dcddb3200e874798c22140a8
Chino <40154737+SlothsAreLazyTho@users.noreply.github.com>
2024-09-04 00:15:30 +0200

    Piping mysql commands now

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -12,22 +12,20 @@
 
 #!/bin/sh
 
-SQL_FILE_NAME="setup.sql"
-
 if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
     mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
     
     service mariadb start
-    echo "CREATE USER \"$DB_USER\"@'%' IDENTIFIED BY \"$DB_PASS\";" >> $SQL_FILE_NAME
-    echo "CREATE DATABASE $DB_NAME;" >> $SQL_FILE_NAME
-    echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO \"$DB_USER\"@'%' WITH GRANT OPTION;" >> $SQL_FILE_NAME
-    echo "FLUSH PRIVILEGES;" >> $SQL_FILE_NAME
 
-    mysql < setup.sql
-    echo "Successfully added $DB_USER to the database"
+    echo "CREATE USER \"$DB_USER\"@'%' IDENTIFIED BY \"$DB_PASS\";" | mysql
+    echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO \"$DB_USER\"@'%' WITH GRANT OPTION;" | mysql
+    echo "CREATE DATABASE $DB_NAME;" | mysql
+    echo "FLUSH PRIVILEGES;" | mysql
 
     # Cleaning inception
     service mariadb stop
+
+    echo "Successfully added $DB_USER to the database"
 else
     echo "MariaDB was already installed"
 fi
