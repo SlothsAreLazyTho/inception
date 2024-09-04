8b85eca89a284c41d49e4200d60cacd83fc140fc
Chino <40154737+SlothsAreLazyTho@users.noreply.github.com>
2024-09-04 14:55:52 +0200

    Solved issue

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -17,10 +17,14 @@ if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
     
     echo "Starting MySQL in safe mode without network"
     mysqld_safe --skip-networking &
-    sleep 5
+    
+    until mysql -h localhost -u root -e "status" >/dev/null 2>&1; do
+        echo "Waiting for MySQL to be ready..."
+        sleep 5
+    done
 
     # Insert query to the database
-    mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+    mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY "$DB_PASS";"
     mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' WITH GRANT OPTION;"
     mysql -e "CREATE DATABASE $DB_NAME;"
     mysql -e "FLUSH PRIVILEGES;"
@@ -28,8 +32,6 @@ if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
     #Done so turn off the server and lets enable the container with networking now!
     mysqladmin --user=root shutdown
     echo "Successfully added $DB_USER to the database"
-else
-    echo "MariaDB was already installed"
 fi
 
 exec "$@"
